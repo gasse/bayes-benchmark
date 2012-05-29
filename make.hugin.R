1e7daf9b41410bbc8d0d737fd6647884e06c5b87
Maxime Gasse <maxime.gasse@gmail.com>
2012-05-29 13:00:51 +0200

    Now generate networks in the hugin format with the utility included in bnlearn, more accurate?

--- make.hugin.R
+++ make.hugin.R
@@ -3,4 +3,5 @@ source("functions.R")
 
 for (network in names(conf.networks)) {
   gen.hugin(conf.networks[[network]], network, paste("hugin/", network, ".hugin", sep=""))
+  write.net(paste("hugin/", network, "_bis.hugin", sep=""), conf.networks[[network]])
 }
